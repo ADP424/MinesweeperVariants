13724eccc0d4818a79905cfe04b45710a57a8d32
Aidan Dalgarno-Platt <aidan.dalgarnoplatt@delta.com>
2024-05-30 09:55:19 -0400

    Made MinesweeperTile its own file

--- Minesweeper/MinesweeperBoard.py
+++ Minesweeper/MinesweeperBoard.py
@@ -1,57 +1,5 @@
-from enum import Enum
 import random
-import sys
-
-
-class Tile(Enum):
-    NULL = -1  # out of bounds
-    EMPTY = 0
-    MINE = 1
-    NUMBERED = 2
-    NEGATIVE_MINE = 3 # not used by regular Minesweeper
-
-
-class MinesweeperTile:
-    """
-    A single tile on the board, either hidden or unhidden.
-
-    Attributes
-    ----------
-    type : Tile, default: Tile.EMPTY
-        The type of tile the tile is (empty, mine, numbered, etc.).
-
-    value : float, default: 0
-        A float or integer representing the number of mines surrounding the tile (if the tile is itself a mine, value is -1).
-
-    revealed : bool, default: False
-        Whether the tile's value is visible to the player or not.
-
-    flag_planted : int, default: 0
-        Which flag, if any, is planted on the tile.
-        0: No Flag
-        1: Positive Flag
-        2: Negative Flag
-
-    changed_last_move : bool, default: False
-        Whether the tile was updated by the last move or not.
-    """
-
-    def __init__(
-        self,
-        type=Tile.EMPTY,
-        value=0,
-        revealed=False,
-        flag_planted=0,
-        changed_last_move=False,
-    ):
-        self.type = type
-        self.value = value
-        self.revealed = revealed
-        self.flag_planted = flag_planted
-        self.changed_last_move = changed_last_move
-
-        # set the recursion limit way up for my silly reveal tile function
-        sys.setrecursionlimit(100000)
+from Minesweeper.MinesweeperTile import Tile, MinesweeperTile
 
 
 class MinesweeperBoard:
@@ -257,7 +205,9 @@ class MinesweeperBoard:
                 (row + 1, col + 1),
             ]
 
-            self.board[row][col].flag_planted = (self.board[row][col].flag_planted + 1) % 2
+            self.board[row][col].flag_planted = (
+                self.board[row][col].flag_planted + 1
+            ) % 2
             self.board[row][col].changed_last_move = True
 
             # change every numbered surrounding tile by the change value
